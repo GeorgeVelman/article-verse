18003c981067a14937671b347c54ca0136a23e7c
GeorgeVelman <velman.g@mail.ru>
2024-09-25 05:39:02 +0500

    add modal, react portal, change header

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,8 @@
+import { useCallback, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { classNames } from 'shared/lib/classNames/classNames'
+import { Button, ButtonTheme } from 'shared/ui/Button/Button'
+import Modal from 'shared/ui/Modal/Modal'
 import cls from './Navbar.module.scss'
 
 interface NavbarProps {
@@ -8,12 +11,28 @@ interface NavbarProps {
 
 export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation()
+    const [isOpen, setIsOpen] = useState(false)
+    const [isAuthModal, setIsAuthModal] = useState(false)
+
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal((prev) => !prev)
+    }, [])
 
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <div className={cls.links}>
-                $$$$$
-            </div>
+            <Button
+                theme={ButtonTheme.CLEAR_INVERTED}
+                className={cls.links}
+                onClick={onToggleModal}
+            >
+                {t('Войти')}
+            </Button>
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                {/*eslint-disable-line*/}
+                Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua.
+            </Modal>
         </div>
     )
 }
