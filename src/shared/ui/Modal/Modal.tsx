e3b7efe3be3bf3033b36c1849b850a51e1142165
GeorgeVelman <velman.g@mail.ru>
2024-09-30 04:34:27 +0500

    fix theme for modal

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -60,7 +60,6 @@ const Modal = (props: ModalProps) => {
     const mods: Record<string, boolean> = {
         [cls.opened]: isOpen,
         [cls.isClosing]: isClosing,
-        [cls[theme]]: true,
     }
 
     return (
